4f79a0721565336c2e365da3c0ad96ca7240d4a6
nair0502 <vaishnavimohanadasnair@gmail.com>
2025-08-09 17:23:28 +0200

    Update genie3_slurm.sh

--- genie3_slurm.sh
+++ genie3_slurm.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=112
 #SBATCH --mem=150G
 #SBATCH --job-name=genie3_job
-#SBATCH -o /dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/thesis/vaishhnavi_master-thesis/slurm_updated_genie/genie3_job.%j.out
-#SBATCH -e /dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/thesis/vaishhnavi_master-thesis/slurm_updated_genie/genie3_job.%j.err
+#SBATCH -o vaishnavi/thesis/vaishhnavi_master-thesis/slurm_updated_genie/genie3_job.%j.out
+#SBATCH -e vaishnavi/thesis/vaishhnavi_master-thesis/slurm_updated_genie/genie3_job.%j.err
 #SBATCH --clusters=cm4
 #SBATCH --partition=cm4_tiny
 #SBATCH --qos=cm4_tiny
@@ -13,4 +13,4 @@
 
 
 # run the R script
-Rscript /dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/thesis/vaishhnavi_master-thesis/genie3.R
+Rscript vaishnavi/thesis/vaishhnavi_master-thesis/genie3.R
