4f7ed8653e40a417a18a4ab1cc22bacbfdfb5d90
nair0502 <vaishnavimohanadasnair@gmail.com>
2025-08-09 17:24:39 +0200

    Update kallisto_multiqc.sh

--- kallisto_multiqc.sh
+++ kallisto_multiqc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Add MultiQC to PATH
-export PATH=$PATH:/dss/dsshome1/08/ge85jek2/miniconda3/envs/multiqc/bin/multiqc
+export PATH=$PATH:/miniconda3/envs/multiqc/bin/multiqc
 
 # Input directory containing Kallisto results
-Kallisto_results_dir="/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/vaishnavi_masterthesis/kallisto_output"
+Kallisto_results_dir="vaishnavi/vaishnavi_masterthesis/kallisto_output"
 
 # Output directory for MultiQC report
-multiqc_output_dir="/dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/vaishnavi_masterthesis/kallisto_multiqc_report"
+multiqc_output_dir="vaishnavi/vaishnavi_masterthesis/kallisto_multiqc_report"
 
 # Ensure output directory exists
 mkdir -p "$multiqc_output_dir"
