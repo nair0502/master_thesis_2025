d048a094197a3769cd73ffea509ae886aae5be88
nair0502 <vaishnavimohanadasnair@gmail.com>
2025-08-09 17:23:44 +0200

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PATH=$PATH:/dss/dsshome1/08/ge85jek2/miniconda3/envs/ncbi/bin/kallisto
+PATH=$PATH:....miniconda3/envs/ncbi/bin/kallisto
 kallisto index -i tomato_index.idx ITAG4.0_cDNA.fasta
 
