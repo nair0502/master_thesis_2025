be95352b6bb7d014f6acfaa091b5d4a05025b9e1
nair0502 <vaishnavimohanadasnair@gmail.com>
2025-08-09 17:22:03 +0200

    Update fastp.sh

--- fastp.sh
+++ fastp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PATH=$PATH:/dss/dsshome1/08/ge85jek2/sratoolkit.3.1.1-centos_linux64/bin/fasterq-dump
-find /dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/vaishnavi_masterthesis/sra_id -name "*.sra" -exec fasterq-dump {} -O /dss/dssfs03/pn57ba/pn57ba-dss-0001/computational-plant-biology/vaishnavi/vaishnavi_masterthesis/fastq \;
+PATH=$PATH:....../sratoolkit.3.1.1-centos_linux64/bin/fasterq-dump
+find vaishnavi/vaishnavi_masterthesis/sra_id -name "*.sra" -exec fasterq-dump {} -O vaishnavi/vaishnavi_masterthesis/fastq \;
